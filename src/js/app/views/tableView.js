d242565259e05fccd21be1bdfeca074de01f83b3
andymason <andrew.mason@guardian.co.uk>
2014-10-07 13:48:42 +0100

    Styling table viz

--- src/js/app/views/tableView.js
+++ src/js/app/views/tableView.js
@@ -21,9 +21,47 @@ define([
             Backbone.on('fetch:success', this.render, this);
         },
 
+        // TODO: Use border width
+        calcScale: function(data) {
+            var maxDiameter = 500;
+            var deathDiameter;
+            var casesDiamerter;
+
+            var totalDeaths = _.reduce(data, function(memo, d) {
+                return memo + parseInt(d.deaths, 10);
+            }, 0);
+
+            var totalCases = _.reduce(data, function(memo, d) {
+                return memo + parseInt(d.cases, 10);
+            }, 0);
+
+
+            if (totalCases > totalDeaths) {
+                casesDiamerter = maxDiameter;
+                deathDiameter = (totalDeaths / totalCases) * maxDiameter;
+            } else {
+                casesDiamerter = (totalCases / totalDeaths) * maxDiameter;
+                deathDiameter = maxDiameter;
+            }
+
+            console.log(casesDiamerter, deathDiameter, totalCases, totalDeaths);
+
+
+            _.forEach(data, function(row) {
+                row.casesPercentage = (row.cases / totalCases) * 100;
+                row.deathsPercentage = (row.deaths / totalDeaths) * 100;
+                row.casesSize = Math.ceil(casesDiamerter / 100 * row.casesPercentage);
+                row.deathsSize = Math.ceil(deathDiameter / 100 * row.deathsPercentage);
+            });
+        },
+
         render: function() {
             var data = EbolaData.getSheet('Historic cases');
+            if (data) {
+                this.calcScale(data);
+            }
             console.log(data);
+
             this.$el.html(this.template({ data: data }));
             return this;
         }
