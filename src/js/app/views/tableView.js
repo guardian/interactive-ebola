1ccad1225cabfa7a0e268242effe06b065938bab
andymason <andrew.mason@guardian.co.uk>
2014-10-07 17:06:19 +0100

    More table work

--- src/js/app/views/tableView.js
+++ src/js/app/views/tableView.js
@@ -20,32 +20,23 @@ define([
         template: _.template(templateHTML),
 
         initialize: function() {
+            this.MAX_VIZ_WIDTH = 500;
+            this.MAX_WINDOW_WIDTH = 660;
             Backbone.on('fetch:success', this.render, this);
+
+            // Handle resize
+            this.resizeRender = _.debounce(this.render.bind(this), 300);
+            $(window).on('resize', this.resizeRender);
+        },
+
+        getVizWidth: function() {
+            var windowWidth = $(window).width();
+            return windowWidth * 0.333;
         },
 
         // TODO: Use border width
         calcScale: function(data) {
-            var maxDiameter = 350;
-            var deathDiameter;
-            var casesDiamerter;
-
-            var totalDeaths = _.reduce(data, function(memo, d) {
-                return memo + parseInt(d.deaths, 10);
-            }, 0);
-
-            var totalCases = _.reduce(data, function(memo, d) {
-                return memo + parseInt(d.cases, 10);
-            }, 0);
-
-
-            if (totalCases > totalDeaths) {
-                casesDiamerter = maxDiameter;
-                deathDiameter = (totalDeaths / totalCases) * maxDiameter;
-            } else {
-                casesDiamerter = (totalCases / totalDeaths) * maxDiameter;
-                deathDiameter = maxDiameter;
-            }
-
+            var maxDiameter = this.getVizWidth();
             var maxRow = _.max(data, function(row) { return row.cases; });
             var maxDiamter = Math.sqrt(maxRow.cases / Math.PI) * 2;
 
@@ -55,24 +46,17 @@ define([
                     row.year = '2001/02';
                 }
 
-                //var diameter = (row.cases / maxRow.cases) * maxDiameter;
                 var diameter = Math.sqrt(row.cases / Math.PI) * 2;
                 var pixelDiameter = (diameter / maxDiamter) * maxDiameter;
-
                 var deathDiameter = Math.sqrt(row.deaths / Math.PI) * 2;
                 var pixelDeathSize = pixelDiameter - ((deathDiameter / maxDiamter) * maxDiameter);
-
-                /*
-                row.casesPercentage = (row.cases / totalCases) * 100;
-                row.deathsPercentage = (row.deaths / totalDeaths) * 100;
-                */
 
                 row.casesSize = Math.ceil(pixelDiameter);
                 row.deathsSize = pixelDeathSize / 2;
 
                 // Format numbers
                 row.cases = numeral(row.cases).format('0,0');
-                row.deaths = numeral(row.cases).format('0,0');
+                row.deaths = numeral(row.deaths).format('0,0');
             });
         },
 
@@ -81,7 +65,6 @@ define([
             if (data) {
                 this.calcScale(data);
             }
-            console.log(data);
 
             this.$el.html(this.template({ data: data }));
             return this;
